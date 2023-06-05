c8af8a5c553025b30a993f62c505ba30745156f5
“Emeka <ngwe7236@gmail.com>
2023-06-06 01:28:13 +0200

    not-finished

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,22 @@
 //  Add your code here
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.set('strictQuery', false);
+const celebritySchema = new Schema({
+  name:{
+    type: String,
+  },
+
+ occupation: {
+    type: String,
+  
+  },
+catchPhrase:{
+    type: String,
+  }
+
+});
+
+const Celebrity = mongoose.model('Celebrities', celebritySchema);
+
+module.exports = Celebrity;
---
+++ routes/celebrities.routes.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+
+// all your routes here
+const Celebrity = require("../models/Celebrity.model");
+
+
+
+
+  
+router.get("/celebrities/create", (req, res) => res.render("celebrities/new-celebrity"));
+
+
+router.post("/celebrities/create", (req, res)=> {
+    const { name, occupation, catchPhrase} = req.body;
+    console.log(req.body)
+
+    Celebrity.findOne({name, occupation, catchPhrase})
+    .then((celebritypost) => {
+       if(!celebritypost) {
+        Celebrity.create({name, occupation, catchPhrase})
+        .then(()=> res.redirect('celebrities/new-celebrity'))
+
+       }else{
+        res.render('celebrities/celebrities');
+        return
+       }
+        })      
+.catch((err)=> console.log(`Error wile creating a new user: ${err}`))
+});
+
+router.get("celebrities", (req, res)=> {
+  Celebrity.find()
+    .then((celeFromDB) => res.render("/celebrities/celebrities", {celebrities: celeFromDB}))
+    .catch((err) => console.log(`Error while getting list of celebrities from the DB: ${err}`));
+});
+module.exports = router;
